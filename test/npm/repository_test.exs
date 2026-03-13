b72f292a90cc6f19e85a0b511b1a88137b910017
Danila Poyarkov <dev@dannote.net>
2026-03-13 18:18:20 +0300

    Add NPM.Repository module (extract, browse_url, clone_url, provider, has_repository?) with 17 tests for repository field parsing and provider detection\n\nResult: {"status":"keep","test_count":1953}

---
+++ test/npm/repository_test.exs
@@ -0,0 +1,108 @@
+defmodule NPM.RepositoryTest do
+  use ExUnit.Case, async: true
+
+  @github_pkg %{
+    "repository" => %{
+      "type" => "git",
+      "url" => "git+https://github.com/lodash/lodash.git"
+    }
+  }
+
+  @monorepo_pkg %{
+    "repository" => %{
+      "type" => "git",
+      "url" => "https://github.com/babel/babel.git",
+      "directory" => "packages/babel-core"
+    }
+  }
+
+  describe "extract" do
+    test "extracts object repository" do
+      repo = NPM.Repository.extract(@github_pkg)
+      assert repo.type == "git"
+      assert repo.url == "https://github.com/lodash/lodash"
+    end
+
+    test "extracts string shorthand" do
+      data = %{"repository" => "github:user/repo"}
+      repo = NPM.Repository.extract(data)
+      assert repo.url == "https://github.com/user/repo"
+    end
+
+    test "extracts bare user/repo" do
+      data = %{"repository" => "user/repo"}
+      repo = NPM.Repository.extract(data)
+      assert repo.url == "https://github.com/user/repo"
+    end
+
+    test "extracts monorepo with directory" do
+      repo = NPM.Repository.extract(@monorepo_pkg)
+      assert repo.directory == "packages/babel-core"
+    end
+
+    test "nil for missing repository" do
+      assert nil == NPM.Repository.extract(%{})
+    end
+  end
+
+  describe "browse_url" do
+    test "returns clean URL" do
+      assert "https://github.com/lodash/lodash" = NPM.Repository.browse_url(@github_pkg)
+    end
+
+    test "includes directory for monorepo" do
+      url = NPM.Repository.browse_url(@monorepo_pkg)
+      assert url =~ "packages/babel-core"
+    end
+
+    test "nil for no repository" do
+      assert nil == NPM.Repository.browse_url(%{})
+    end
+  end
+
+  describe "clone_url" do
+    test "returns URL with .git suffix" do
+      url = NPM.Repository.clone_url(@github_pkg)
+      assert String.ends_with?(url, ".git")
+    end
+
+    test "nil for no repository" do
+      assert nil == NPM.Repository.clone_url(%{})
+    end
+  end
+
+  describe "provider" do
+    test "detects github" do
+      assert :github = NPM.Repository.provider(@github_pkg)
+    end
+
+    test "detects gitlab" do
+      data = %{"repository" => %{"type" => "git", "url" => "https://gitlab.com/user/repo"}}
+      assert :gitlab = NPM.Repository.provider(data)
+    end
+
+    test "detects bitbucket" do
+      data = %{"repository" => %{"type" => "git", "url" => "https://bitbucket.org/user/repo"}}
+      assert :bitbucket = NPM.Repository.provider(data)
+    end
+
+    test "other for unknown host" do
+      data = %{"repository" => %{"type" => "git", "url" => "https://custom.host/repo"}}
+      assert :other = NPM.Repository.provider(data)
+    end
+
+    test "nil for no repository" do
+      assert nil == NPM.Repository.provider(%{})
+    end
+  end
+
+  describe "has_repository?" do
+    test "true with repository" do
+      assert NPM.Repository.has_repository?(@github_pkg)
+    end
+
+    test "false without repository" do
+      refute NPM.Repository.has_repository?(%{})
+    end
+  end
+end
